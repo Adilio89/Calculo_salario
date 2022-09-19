f2afd48e4a4335836c6b47967bc90b94c632809d
Adilio89 <88595417+Adilio89@users.noreply.github.com>
2022-09-19 11:50:06 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculo_salario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sb, inss, vt, sl;
+            sb = Convert.ToDouble(textBox3.Text);
+            inss = sb * 0.11;
+            textBox4.Text = inss.ToString("#,##0.00");
+            vt = sb * 0.06;
+            textBox5.Text = vt.ToString("#,##0.00");
+            sl = sb - (inss + vt);
+            textBox6.Text = sl.ToString("#,##0.00");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            pictureBox1.Image = Properties.Resources.sem_imagem;
+            textBox1.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja Finalizar", "Confirme",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question) == DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            pictureBox1.Load(openFileDialog1.FileName);
+        }
+    }
+}
